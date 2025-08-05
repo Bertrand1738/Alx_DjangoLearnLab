c003a146b1031cce2480d915f25240e7e1054eaa
Bertrand1738 <hbertrand605@gmail.com>
2025-08-05 17:25:33 +0300

    Refactor project structure: delete unused API files and update settings for proper module path

--- api_project/urls.py
+++ api_project/urls.py
@@ -1,5 +1,4 @@
-"""
-URL configuration for api_project project.
+"""api_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/

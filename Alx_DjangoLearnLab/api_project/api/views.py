ad0e89dbb26a9a49c2ee7b4f457d7d9de20f5880
Bertrand1738 <hbertrand605@gmail.com>
2025-08-05 18:36:42 +0300

    Implement initial API views and serializers for Book model

--- Alx_DjangoLearnLab/api_project/api/views.py
+++ Alx_DjangoLearnLab/api_project/api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+
+from django.http import JsonResponse
+
+def book_list(request):
+    data = {
+        "books": [
+            {"id": 1, "title": "Django for Beginners"},
+            {"id": 2, "title": "Python Crash Course"}
+        ]
+    }
+    return JsonResponse(data)
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
 # Create your views here.

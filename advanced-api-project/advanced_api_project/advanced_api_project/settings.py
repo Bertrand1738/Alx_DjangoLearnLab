d137784a9e8464a3865eb883cc5106b15a04f805
Bertrand <159031663+Bertrand1738@users.noreply.github.com>
2025-08-11 15:27:29 +0300

    Update settings.py

--- advanced-api-project/advanced_api_project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/advanced_api_project/settings.py
@@ -37,9 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-        'api',  # Added 'api' to INSTALLED_APPS
-    # Third-party apps
-    'rest_framework',
+     'rest_framework',  
+     'api',  
    
     
 ]

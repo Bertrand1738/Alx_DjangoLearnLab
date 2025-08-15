674b08c512bfc2bd74ee6d977f3760bd9c7e59ca
Bertrand1738 <hbertrand605@gmail.com>
2025-08-15 10:03:11 +0300

    Update blog app registration to use full app config path

--- django-blog-project/django_blog/django_blog/settings.py
+++ django-blog-project/django_blog/django_blog/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',  # Add the blog app
+    'blog.apps.BlogConfig',  # Use full app config path
 ]
 
 MIDDLEWARE = [

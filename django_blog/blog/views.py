532c94c2b6b727f2da4c1c86f071d33411b2ef9a
Bertrand1738 <hbertrand605@gmail.com>
2025-08-16 12:10:04 +0300

    Add user authentication features and blog functionality
    - Implemented user registration, login, and logout views.
    - Created templates for user registration and login.
    - Added profile view for authenticated users to update their information.
    - Developed blog post model with fields for title, content, published date, and author.
    - Created admin interface for managing blog posts.
    - Added views and templates for displaying a list of blog posts.
    - Implemented basic styling for the blog using CSS.
    - Added unit tests for user authentication and blog functionality.
    - Updated project settings to include the new blog app.

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -3,6 +3,11 @@ from django.contrib.auth import login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
 from .forms import CustomUserCreationForm, UserUpdateForm
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import Post
+from .forms import PostForm
+from django.urls import reverse_lazy
 
 def register_view(request):
     if request.method == "POST":
@@ -40,3 +45,48 @@ def profile_view(request):
     else:
         form = UserUpdateForm(instance=request.user)
     return render(request, "blog/profile.html", {"form": form})
+
+# ListView: Shows all posts
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-published_date']
+
+# DetailView: Shows a single post
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
+# CreateView: Authenticated users can create posts
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+    success_url = reverse_lazy('post-list')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+# UpdateView: Only author can edit
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+    success_url = reverse_lazy('post-list')
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+# DeleteView: Only author can delete
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('post-list')
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author

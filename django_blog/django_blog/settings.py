09f8ed88987bb2cc458878e5de9ffbdc1c7ba355
Bertrand1738 <hbertrand605@gmail.com>
2025-08-18 15:18:15 +0300

    Fix blog app installation in INSTALLED_APPS

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog.apps.BlogConfig',  # proper way to register the app
+    'blog',
 ]
 
 MIDDLEWARE = [
